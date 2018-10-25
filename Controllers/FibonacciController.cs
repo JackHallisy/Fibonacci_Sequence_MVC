529734e676f818c334776bbfaaf7ad4e42e3b3a9
JackHallisy <39491444+JackHallisy@users.noreply.github.com>
2018-10-25 18:46:07 -0400

    Add files via upload

---
+++ Controllers/FibonacciController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FibonacciSequenceMVC.Controllers
+{
+    public class FibonacciController : Controller
+    {
+        // GET: Fibonacci
+        public ActionResult Index()
+        {                              
+            int sum = 1;
+            int total = 0;
+            int newTotal = 0;
+
+            for (int ctr = 1; ctr < 21; ctr++)
+            {
+                total = newTotal + sum;
+                newTotal = sum;
+                sum = total;
+            }
+
+            ViewBag.total = total;
+
+            return View();
+        }
+    }
+}
